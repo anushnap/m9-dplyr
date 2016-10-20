b45c79b5d905e780b21632fa37542007b4c744d5
Anushna Prakash <anushnap@uw.edu>
2016-10-20 14:51:07 -0700

    attempted exercise 5

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,13 +1,15 @@
 # Exercise 5: DPLYR Grouped Operations
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
+require(dplyr)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+flights %>% group_by(month) %>% summarize(avg.dep.delay = mean(dep_delay, na.rm = TRUE)) %>%
+  plot()
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
@@ -15,7 +17,7 @@ library(dplyr)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+flights %>% group_by(dest) %>% summarize(avg.arrival.delay = mean(arr_delay, na.rm = TRUE)) %>% arrange(desc(avg.arrival.delay))
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
